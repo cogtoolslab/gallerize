087d7b7b52cd8e164da5ab961d0a91368826fd8c
yiy142 <yiy142@ucsd.edu>
2019-06-27 14:56:36 -0700

    different sorting algorithm done

--- src/Layout.js
+++ src/Layout.js
@@ -2,10 +2,57 @@ import React from 'react';
 import { Select, Layout, Card} from 'element-react';
 import 'element-theme-default';
 import './Layout.css';
-import { resolve, reject, async } from 'q';
 
+
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      option: "dummy"
+    }
+  }
+  handleOptionChange(newOption){
+    console.log("in Main, state changed! ");
+    this.state.option = newOption;
+    this.setState({option : newOption});
+    console.log(this.state.option);
+    this.render();
+  }
+
+  render(){
+    let css1 = {
+      float: 'left',
+      paddingLeft: '50px'
+    }
+    let css2 = {
+      fontSize: '25px',
+      fontWeight: 'bold'
+    }
+    let css3 = {
+      float:'right',
+      padding: '10px 50px'
+    }
+    return (
+
+      <div>
+              {console.log("in main render")}
+        <div className="header">
+          <div style = {css1}>
+            <a href="#" style={css2}>CogToolsLabLogo</a>
+          </div>
+
+          <div style={css3} >
+            <SelectSort onSelectChange = {this.handleOptionChange.bind(this)}> </SelectSort>
+          </div>
+        </div>
+        {console.log(this.state.option)}
+        <CardLayout order = {this.state.option}> </CardLayout>
+      </div>
+    )
+  }
+}
 /**
- * The Layout Component.
+ * The Layout Component. Did not use any async calls
  * TODO: Need to add "render upon scrolling"
  * TODO: Need some better async control...
  */
@@ -13,17 +60,16 @@ class CardLayout extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: this.props.value,
       array:[],
       toRender: [],
-      order: "option1",
+      order: this.props.order,
       toRet: []
     };
     this.readCSV("/parser/parsed.csv");
-    this.renderOrder("option1");
   }
 
   render() {
+    this.renderOrder();
     return (
       <div className="content">
         {console.log("started rendering")}
@@ -33,17 +79,48 @@ class CardLayout extends React.Component {
     )
   }
 
-  renderOrder(option){
-    console.log("in renderOrder");
-    //Sort toRender Array
-    if (option === "option1"){
-      this.state.array.sort((a, b) => (parseInt(a.age) > parseInt(b.age)) ? 1 : -1);
+  compare_age(a,b){
+    if(this.state.order === "option1"){
+      if (parseInt(a.age) - parseInt(b.age) != 0)
+          return (parseInt(a.age) - parseInt(b.age));
+      return ((a.class) > (b.class) ? 1: (b.class) > (a.class)? -1: 0); 
     }
+    else if (this.state.order === "option2"){
+      if (parseInt(b.age) - parseInt(a.age) != 0)
+          return (parseInt(b.age) - parseInt(a.age));
+      return ((a.class) > (b.class) ? 1: (b.class) > (a.class)? -1: 0); 
+    }
+  }
+  compare_class(a,b){
+    if(this.state.order === "option3"){
+      if (a.class !== b.class)
+          return ((a.class) > (b.class) ? 1: (b.class) > (a.class)? -1: 0);
+      return (parseInt(a.age) - parseInt(b.age));
+    }
+    else if (this.state.order === "option4"){
+        if (a.class !== b.class)
+            return ((a.class) < (b.class) ? 1: (b.class) < (a.class)? -1: 0);
+        return (parseInt(a.age) - parseInt(b.age));
+    }
+  }
 
+  renderOrder(){
+    this.state.order = this.props.order;
+    let option = this.props.order;
+    console.log(option);
+    //Sort toRender Array
+    if (option === "option1" || option === "option2"){
+      this.state.array.sort(this.compare_age.bind(this));
+    }
+    else if (option === "option3" || option === "option4"){
+      this.state.array.sort(this.compare_class.bind(this));
+    }
+    this.state.toRender = []
     this.state.array.forEach(function (item){
       if(item.filename)
         this.state.toRender.push("" + item.filename);
     }, this);
+    this.state.toRet = []
     /* create HTML nodes for render function */
     this.state.array.forEach(function (item){
       if(item.filename)
@@ -74,8 +151,11 @@ class CardLayout extends React.Component {
           let allTextLines = allText.split('\n');
           allTextLines.forEach(function (item){
             let row = item.split(',');
-            let col = {"class": row[0], "age": row[1], "expID": row[2], "sessionID": row[3], "filename": row[4], valid: row[5]};
-            this.state.array.push(col);
+            /* we want to skip the first row*/
+            if (row[0] !=="class"){
+              let col = {"class": row[0], "age": row[1], "expID": row[2], "sessionID": row[3], "filename": row[4], valid: row[5]};
+              this.state.array.push(col);
+            }
           }, this);
           console.log("done reading");
         }
@@ -96,17 +176,17 @@ constructor(props) {
   
     this.state = {
       options: [{
-        value: 'Option1',
+        value: 'option1',
         label: 'Age (Young - Old)'
       }, {
-        value: 'Option2',
+        value: 'option2',
         label: 'Age (Old - Young)'
       }, {
-        value: 'Option3',
-        label: 'Date'
+        value: 'option3',
+        label: 'Class (A - Z)'
       }, {
-        value: 'Option4',
-        label: 'Class'
+        value: 'option4',
+        label: 'Class (Z - A)'
       }],
       value: ''
     };
@@ -114,7 +194,7 @@ constructor(props) {
   
   render() {
     return (
-      <Select placeholder = "sort by ..." value={this.state.value}>
+      <Select onChange = {this.handleChange.bind(this)} placeholder = "sort by ..." value={this.state.value}>
         {
           this.state.options.map(el => {
             return <Select.Option  key={el.value} label={el.label} value={el.value} />
@@ -123,6 +203,12 @@ constructor(props) {
       </Select>
     )
   }
+
+  handleChange(event){
+    console.log("selection changed! ");
+    console.log(event);
+    this.props.onSelectChange(event);
+  }
 }
 
-export {SelectSort, CardLayout};
+export {SelectSort, CardLayout, Main};
