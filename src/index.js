f6268854ddea6717d06bc9337c0d28aa4f69dcdb
yiy142 <yiy142@ucsd.edu>
2019-07-09 20:01:11 -0700

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Main } from "./Main";
+import { Main } from "./components/Main";
 import "element-theme-default";
 import * as serviceWorker from "./serviceWorker";
 
