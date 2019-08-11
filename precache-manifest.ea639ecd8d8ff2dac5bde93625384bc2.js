a5bd5b8e449624d401b9b68c9717f30e61991006
yiy142 <yiy142@ucsd.edu>
2019-08-10 23:06:11 -0700

    Updates

---
+++ precache-manifest.ea639ecd8d8ff2dac5bde93625384bc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8748218b219ceaa904c849c10e23b7a7",
+    "url": "/gallerize/index.html"
+  },
+  {
+    "revision": "05496681f702ca6de0a3",
+    "url": "/gallerize/static/css/2.815e7c76.chunk.css"
+  },
+  {
+    "revision": "490688f029502f68c659",
+    "url": "/gallerize/static/css/main.2489a7eb.chunk.css"
+  },
+  {
+    "revision": "05496681f702ca6de0a3",
+    "url": "/gallerize/static/js/2.bec7b3a5.chunk.js"
+  },
+  {
+    "revision": "490688f029502f68c659",
+    "url": "/gallerize/static/js/main.21d46a81.chunk.js"
+  },
+  {
+    "revision": "19d79652df26119a15f5",
+    "url": "/gallerize/static/js/runtime~main.b053e6a8.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/gallerize/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/gallerize/static/media/element-icons.d2f69a92.woff"
+  }
+]);
