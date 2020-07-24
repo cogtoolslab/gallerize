f31453aef5eb64ac206bb3f40ec9ffa058ef4472
Bria Long <brialorelle@gmail.com>
2020-07-24 13:08:33 -0700

    add consent statement

--- src/components/mturk_study/mturk.js
+++ src/components/mturk_study/mturk.js
@@ -75,6 +75,12 @@ class MTurk extends React.Component {
             arrows, or other symbols that are not part of the object itself.
             <br />We expect this hit to take approximately 10-15 minutes to complete, 
             including the time it takes to read instructions.
+            <br /> By answering the following questions, you are participating in a study being performed by cognitive scientists in the Stanford Department of Psychology.
+            You must be at least 18 years old to participate. Your participation in this research is voluntary.
+            You may decline to answer any or all of the following questions. You may decline further participation, at any time, without adverse consequences.
+            Your anonymity is assured; the researchers who have requested your participation will not receive any personal information about you.
+            Note however that we have recently been made aware that your public Amazon.com profile can be accessed via your worker ID if you do not choose to opt out.
+            If you would like to opt out of this feature, you may follow instructions available <a href="https://www.amazon.com/gp/help/customer/display.html?nodeId=16465241">here</a>
             <br />If you encounter a problem or error, send us an email (bria@stanford.edu) 
             and we will make sure you are compensated for your time! Please pay attention and 
             do your best! Thank you!
